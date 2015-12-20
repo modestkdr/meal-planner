45bea2a1335ab79fdb9ab75733a92ee95c2eeae9
Srikanth AD <adsrikanth@gmail.com>
2015-12-20 12:08:38 -0500

    meal calendar now displays month/dates for sections and hour of the day for recipes/items in each section

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -10,6 +10,7 @@ var Calendar = React.createClass({
 			if(items[index].planned_for.indexOf(dateStr) > -1) {
 				result.push(
 					<li key={index}>
+						<div>{moment(items[index].planned_for).format('hA')}</div>
 						<p>{items[index].text}</p>
 					</li>
 				);
@@ -21,21 +22,24 @@ var Calendar = React.createClass({
 	render() {
 		return (
 			<section className="col-md-8">
-				<h3>{this.props.headingText}</h3>
+				<h3>{this.props.headingText}</h3>				
 				<section className="col-sm-6 col-md-4 calendar-day">
-					<h4>Today</h4>
+					<h4>Today ({moment().add(0, 'days').format('MMM Do')})</h4>
+					<hr/>
 					<ul className="list-unstyled">
 						{this.getItems(moment().add(0, 'days').format('YYYY-MM-DD'))}
 					</ul>
 				</section>
 				<section className="col-sm-6 col-md-4 calendar-day">
-					<h4>Tomorrow</h4>
+					<h4>Tomorrow ({moment().add(1, 'days').format('MMM Do')})</h4>
+					<hr/>
 					<ul className="list-unstyled">
 						{this.getItems(moment().add(1, 'days').format('YYYY-MM-DD'))}
 					</ul>					
 				</section>
 				<section className="col-sm-6 col-md-4 calendar-day">
-					<h4>Day After Tomorrow</h4>
+					<h4>Day After Tomorrow ({moment().add(2, 'days').format('MMM Do')})</h4>
+					<hr/>
 					<ul className="list-unstyled">
 						{this.getItems(moment().add(2, 'days').format('YYYY-MM-DD'))}
 					</ul>					
